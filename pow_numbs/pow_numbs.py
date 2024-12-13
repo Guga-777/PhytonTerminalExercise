d3c494afe6218c738c2d5c02ad3ed4c93927407e
Guga-777 <gugadevidze2020@gmail.com>
2024-12-13 16:28:30 +0400

    first commit

---
+++ pow_numbs/pow_numbs.py
@@ -0,0 +1,6 @@
+i = int(input('please enter number :'))
+a = input('do you want to get this number in second pow? (y/n) :')
+if a == 'y' :
+   print (i*i)
+else :
+   print('okey, have a nice day')   
